d01f4a41db19a7097000ade4e5733b1f7d4a38b6
Acep Aris Mubarok <acep@radyalabs.com>
2020-03-29 16:51:01 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler'
+import * as React from 'react';
+import {
+  NavigationContainer
+} from '@react-navigation/native';
+
+function App() {
+  return (
+    <NavigationContainer>
+      {/* {mycode} */}
+    </NavigationContainer>
+  );
+}
+
+export default App();
