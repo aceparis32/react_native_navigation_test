969615825ab48ddb7de43995c3a004e93a722534
Acep Aris Mubarok <aceparis32@gmail.com>
2020-03-29 21:15:43 +0700

    Moving between screens

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import {
   View,
-  Text
+  Text,
+  Button
 } from 'react-native';
 import {
   NavigationContainer
@@ -11,19 +12,45 @@ import {
 } from '@react-navigation/stack';
 
 // make home screen page
-function HomeScreen(){
+// add navigation param to each function for navigating between screen
+function HomeScreen({ navigation }){
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+      <Button 
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
 
 // make details screen page
-function DetailsScreen(){
+// add navigation param to each function for navigating between screen
+function DetailsScreen({ navigation }){
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      {/* push will make a new screen altough we're currently on that screen */}
+      <Button 
+        title="Go to Details...again"
+        onPress={() => navigation.push('Details')}
+      />
+      {/* normally we use navigate to navigating to existing screen */}
+      <Button 
+        title="Go to Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+      {/* use goBack() function to return to previous screen */}
+      <Button
+        title="Go back"
+        onPress={() => navigation.goBack()}
+      />
+      {/* use popToPop() function to return to first screen */}
+      <Button
+        title="Go back to first screen"
+        onPress={() => navigation.popToTop()}
+      />
     </View>
   );
 }
@@ -53,5 +80,12 @@ function App(){
     </NavigationContainer>
   );
 }
+
+// CONCLUSSIONS
+// - navigation.navigate('RouteName') pushes a new route to the stack navigator if it's not already in the stack, otherwise it jumps to that screen.
+// - We can call navigation.push('RouteName') as many times as we like and it will continue pushing routes.
+// - The header bar will automatically show a back button, but you can programmatically go back by calling navigation.goBack(). On Android, the hardware back button just works as expected.
+// - You can go back to an existing screen in the stack with navigation.navigate('RouteName'), and you can go back to the first screen in the stack with navigation.popToTop().
+// - The navigation prop is available to all screen components (components defined as screens in route configuration and rendered by React Navigation as a route).
 
 export default App;
